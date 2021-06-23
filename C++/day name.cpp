96d58705de7794f5deda95d74f53cc794ed0f5de
Jasmaine Khale <77222876+adjascently@users.noreply.github.com>
2021-06-23 19:52:41 +0530

    Create day name.cpp

---
+++ C++/day name.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+void DayName(int day)
+{
+   if(day==1)
+        cout<<"sun";
+    else if(day==2)
+        cout<<"mon";
+    else if(day==3)
+        cout<<"tue";
+    else if(day==4)
+        cout<<"wed";
+    else if(day==5)
+        cout<<"thu";
+    else if(day==6)
+        cout<<"fri";
+    else
+        cout<<"sat";
+    
+} 
+    
